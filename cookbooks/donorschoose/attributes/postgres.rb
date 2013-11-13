60b62e176268c6034c0ec41c97f4e7d187a8caaa
Shawn Catanzarite <me@shawncatz.com>
2013-11-12 19:56:29 -0800

    add root account

--- cookbooks/donorschoose/attributes/postgres.rb
+++ cookbooks/donorschoose/attributes/postgres.rb
@@ -4,6 +4,9 @@ default['postgresql']['password']['postgres'] = 'nr628rh7uv6f'
 
 default['postgres']['password'] = 'm9wurarfs4qy'
 default['postgres']['users'] = {
+    root: {
+        password: 'root',
+    },
     app: {
         password: 'm9wurarfs4qy',
     }
